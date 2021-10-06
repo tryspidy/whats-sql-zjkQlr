366c5e1fd49501097a44bcb5ea2a2fd3053344d9
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-06 22:22:26 +0530

    Initial commit

---
+++ main.ts
@@ -0,0 +1,5 @@
+SQL stands for Structured Query Language. It’s the language of choice on today’s web for storing,
+manipulating and retrieving data within relational databases. Most, if not all of the websites you visit
+will use it in some way.
+Using SQL, you are able to interact with the database by writing queries, which when executed,
+return any results which meet its criteria.
